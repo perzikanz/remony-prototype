cab67bcd37329e45cae531518472a20cf1206b5e
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-10-09 17:37:32 +0900

    フリー演奏のページにピアノのSVGを追加

---
+++ src/pages/Free.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { pianoSvg } from '../piano/piano';
+
+export default function Free() {
+  return <>{pianoSvg}</>;
+}
---
+++ src/piano/piano.js
@@ -0,0 +1,480 @@
+import React from 'react';
+import './piano.css';
+
+export const pianoSvg = (
+  <div className="keyboad">
+    <svg width="4100" height="450">
+      <g>
+        <path
+          d="M 50,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c0"
+        />
+        <path
+          d="M 100,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs0"
+        />
+        <path
+          d="M 150,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d0"
+        />
+        <path
+          d="M 190,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds0"
+        />
+        <path
+          d="M 240,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e0"
+        />
+        <path
+          d="M 290,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f0"
+        />
+        <path
+          d="M 340,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs0"
+        />
+        <path
+          d="M 390,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g0"
+        />
+        <path
+          d="M 425,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs0"
+        />
+        <path
+          d="M 475,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z"
+          id="a0"
+          className="white"
+        />
+        <path
+          d="M 510,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as0"
+        />
+        <path
+          d="M 560,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b0"
+        />
+        <text x="68" y="400" className="text">
+          C0
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 610,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c1"
+        />
+        <path
+          d="M 660,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs1"
+        />
+        <path
+          d="M 710,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d1"
+        />
+        <path
+          d="M 750,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds1"
+        />
+        <path
+          d="M 800,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e1"
+        />
+        <path
+          d="M 850,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f1"
+        />
+        <path
+          d="M 900,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs1"
+        />
+        <path
+          d="M 950,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g1"
+        />
+        <path
+          d="M 985,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs1"
+        />
+        <path
+          d="M 1035,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z"
+          className="white"
+          id="a1"
+        />
+        <path
+          d="M 1070,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as1"
+        />
+        <path
+          d="M 1120,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b1"
+        />
+        <text x="628" y="400" className="text">
+          C1
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 1170,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c2"
+        />
+        <path
+          d="M 1220,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs2"
+        />
+        <path
+          d="M 1270,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d2"
+        />
+        <path
+          d="M 1310,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds2"
+        />
+        <path
+          d="M 1360,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e2"
+        />
+        <path
+          d="M 1410,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f2"
+        />
+        <path
+          d="M 1460,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs2"
+        />
+        <path
+          d="M 1510,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g2"
+        />
+        <path
+          d="M 1545,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs2"
+        />
+        <path
+          d="M 1595,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z"
+          className="white"
+          id="a2"
+        />
+        <path
+          d="M 1630,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as2"
+        />
+        <path
+          d="M 1680,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b2"
+        />
+        <text x="1188" y="400" className="text">
+          C2
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 1730,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c3"
+        />
+        <path
+          d="M 1780,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs3"
+        />
+        <path
+          d="M 1830,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d3"
+        />
+        <path
+          d="M 1870,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds3"
+        />
+        <path
+          d="M 1920,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e3"
+        />
+        <path
+          d="M 1970,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f3"
+        />
+        <path
+          d="M 2020,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs3"
+        />
+        <path
+          d="M 2070,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g3"
+        />
+        <path
+          d="M 2105,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs3"
+        />
+        <path
+          d="M 2155,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z"
+          className="white"
+          id="a3"
+        />
+        <path
+          d="M 2190,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as3"
+        />
+        <path
+          d="M 2240,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b3"
+        />
+        <text x="1748" y="400" className="text">
+          C3
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 2290,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c4"
+        />
+        <path
+          d="M 2340,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs4"
+        />
+        <path
+          d="M 2390,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d4"
+        />
+        <path
+          d="M 2430,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds4"
+        />
+        <path
+          d="M 2480,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e4"
+        />
+        <path
+          d="M 2530,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f4"
+        />
+        <path
+          d="M 2580,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs4"
+        />
+        <path
+          d="M 2630,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g4"
+        />
+        <path
+          d="M 2665,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs4"
+        />
+        <path
+          d="M 2715,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z"
+          className="white"
+          id="a4"
+        />
+        <path
+          d="M 2750,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as4"
+        />
+        <path
+          d="M 2800,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b4"
+        />
+        <text x="2308" y="400" className="text">
+          C4
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 2850,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c5"
+        />
+        <path
+          d="M 2900,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs5"
+        />
+        <path
+          d="M 2950,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d5"
+        />
+        <path
+          d="M 2990,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds5"
+        />
+        <path
+          d="M 3040,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e5"
+        />
+        <path
+          d="M 3090,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f5"
+        />
+        <path
+          d="M 3140,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs5"
+        />
+        <path
+          d="M 3190,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g5"
+        />
+        <path
+          d="M 3225,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs5"
+        />
+        <path
+          d="M 3275,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z "
+          id="a5"
+          className="white"
+        />
+        <path
+          d="M 3310,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as5"
+        />
+        <path
+          d="M 3360,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b5"
+        />
+        <text x="2868" y="400" className="text">
+          C5
+        </text>
+      </g>
+
+      <g>
+        <path
+          d="M 3410,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="c6"
+        />
+        <path
+          d="M 3460,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="cs6"
+        />
+        <path
+          d="M 3510,25 v 225 h -20 v 180 h 80 v -225 h -20 v -180 Z"
+          className="white"
+          id="d6"
+        />
+        <path
+          d="M 3550,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="ds6"
+        />
+        <path
+          d="M 3600,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="e6"
+        />
+        <path
+          d="M 3650,25 v 405 h 80 v -180 h -30 v -225 Z"
+          className="white"
+          id="f6"
+        />
+        <path
+          d="M 3700,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="fs6"
+        />
+        <path
+          d="M 3750,25 v 225 h -20 v 180 h 80 v -180 h -25 v -225 Z"
+          className="white"
+          id="g6"
+        />
+        <path
+          d="M 3785,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="gs6"
+        />
+        <path
+          d="M 3835,25 v 225 h -25 v 180 h 80 v -180 h -20 v -225 Z "
+          id="a6"
+          className="white"
+        />
+        <path
+          d="M 3870,25 v 225 h 50 v -225 Z"
+          className="black"
+          id="as6"
+        />
+        <path
+          d="M 3920,25 v 225 h -30 v 180 h 80 v -405 Z"
+          className="white"
+          id="b6"
+        />
+        <text x="3428" y="400" className="text">
+          C6
+        </text>
+      </g>
+      <g>
+        <path
+          d="M 3970,25 v 405 h 80 v -405 Z"
+          className="white"
+          id="c7"
+        />
+        <text x="3988" y="400" className="text">
+          C7
+        </text>
+      </g>
+    </svg>
+  </div>
+);
