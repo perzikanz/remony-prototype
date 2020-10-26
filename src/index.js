8479f049642cfe1d79603d5ea90db3bfd98711f9
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-10-26 18:44:24 +0900

    reduxを追加

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
     <App />
-  </BrowserRouter>,
+  </Provider>,
   document.getElementById('root')
 );
