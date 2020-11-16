9c3d255b1b665fea96e259d43dfdc018e0a18a63
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-11-16 18:29:10 +0900

    リザルト画面の追加

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,10 +1,19 @@
-export const AUK_QUESTIONS = 'AUK_QUESTIONS';
+export const INITIAL_AUDIO = 'INITIAL_AUDIO';
+export const ASK_QUESTIONS = 'ASK_QUESTIONS';
 export const CORRECT_ANSWER = 'CORRECT_ANSWER';
 export const WRONG_ANSWER = 'WRONG_ANSWER';
+export const END_QUENSTION = 'END_QUENSTION';
+export const ONE_MORE_PLAY = 'ONE_MORE_PLAY';
+
+export const initialAudio = () => {
+  return {
+    type: INITIAL_AUDIO,
+  };
+};
 
 export const askQuestions = (correctKey) => {
   return {
-    type: AUK_QUESTIONS,
+    type: ASK_QUESTIONS,
     payload: {
       correctKey,
     },
@@ -20,5 +29,17 @@ export const correctAnswer = () => {
 export const wrongAnswer = () => {
   return {
     type: WRONG_ANSWER,
+  };
+};
+
+export const endQuestion = () => {
+  return {
+    type: END_QUENSTION,
+  };
+};
+
+export const oneMore = () => {
+  return {
+    type: ONE_MORE_PLAY,
   };
 };
