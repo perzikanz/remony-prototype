414b27f2f1729b8df8855b6ef50d072368585984
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-05-20 17:45:56 +0900

    スケールを選ぶボタンを実装

--- src/pages/Chord/Chord.js
+++ src/pages/Chord/Chord.js
@@ -3,11 +3,10 @@ import './chord.css';
 
 import DivKeyboard from 'react-piano-keyboard';
 
-const activeKeys = ['c3', 'g3', 'ds3'];
-
 export function Chord() {
+  const activeKeys = ['c3', 'g3', 'ds3'];
   return (
-    <div>
+    <>
       <h2>Chord(コード演奏)</h2>
       <DivKeyboard
         keyboadLength={1}
@@ -15,6 +14,38 @@ export function Chord() {
         firstScale={3}
         activeKey={activeKeys}
       />
-    </div>
+      <ChooseScale />
+    </>
+  );
+}
+
+function ChooseScale() {
+  return (
+    <>
+      <h3 className='h3'>スケールを選ぶ</h3>
+      <ul className='scale_list'>
+        <li className='scale_list_item'>
+          <button className='scale_button'>C</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>D</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>E</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>F</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>G</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>A</button>
+        </li>
+        <li className='scale_list_item'>
+          <button className='scale_button'>B</button>
+        </li>
+      </ul>
+    </>
   );
 }
