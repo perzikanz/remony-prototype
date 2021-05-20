36bd87159e8fe5af0be8cf9e0a166de121de02bb
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-05-20 15:57:42 +0900

    CSSを調整

--- src/pages/Chord/Chord.js
+++ src/pages/Chord/Chord.js
@@ -3,11 +3,18 @@ import './chord.css';
 
 import DivKeyboard from 'react-piano-keyboard';
 
+const activeKeys = ['c3', 'g3', 'ds3'];
+
 export function Chord() {
   return (
     <div>
       <h2>Chord(コード演奏)</h2>
-      <DivKeyboard keyboadLength={1} keybardWidth={560} />
+      <DivKeyboard
+        keyboadLength={1}
+        keybardWidth={400}
+        firstScale={3}
+        activeKey={activeKeys}
+      />
     </div>
   );
 }
