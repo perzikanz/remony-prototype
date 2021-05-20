4ab5d6ef5d6691f9136829f6df0fa4b47a5a67a0
perzikanz <65535030+perzikanz@users.noreply.github.com>
2021-05-20 13:35:28 +0900

    react-piano-keyboardの追加と表示

--- src/pages/Chord/Chord.js
+++ src/pages/Chord/Chord.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import { Piano, MidiNumbers } from 'react-piano';
-import 'react-piano/dist/styles.css';
-
 import './chord.css';
+
+import DivKeyboard from 'react-piano-keyboard';
 
 export function Chord() {
   return (
     <div>
-      <h2>Chord</h2>
-      <Keyboard />
+      <h2>Chord(コード演奏)</h2>
+      <DivKeyboard keyboadLength={1} keybardWidth={560} />
     </div>
-  );
-}
-
-function Keyboard() {
-  const firstNote = MidiNumbers.fromNote('c3');
-  const lastNote = MidiNumbers.fromNote('a3');
-
-  return (
-    <Piano
-      noteRange={{ first: firstNote, last: lastNote }}
-      playNote={(midiNumber) => {}}
-      stopNote={(midiNumber) => {}}
-      width={300}
-    />
   );
 }
